13898eb622e4f69ce4a5cbb09ff4939c56d5afe6
Simon Szalai <simon.szalai@gmail.com>
2022-12-28 11:17:23 -0300

    modify extract and load for J2

--- io/extract.py
+++ io/extract.py
@@ -2,13 +2,16 @@ import os
 import pandas as pd
 
 
-def extract_table(table_name, pg_conn=None):
+def extract_table(table_name, pg_conn=None, start_time=None, end_time=None):
 
     if pg_conn is None:
         assert os.getenv("PG_CONN") is not None, "PG_CONN environment variable is not set."
         pg_conn = os.getenv("PG_CONN")
 
     query = f"SELECT * FROM {table_name}"
+
+    if start_time is not None and end_time is not None:
+        query += f"WHERE created_at >= '{start_time}' AND created_at < '{end_time}'"
 
     df = pd.read_sql(
         sql=query,
--- io/load.py
+++ io/load.py
@@ -16,10 +16,11 @@ def postgres_upsert(table, conn, keys, data_iter):
         set_={c.key: c for c in insert_statement.excluded},
     )
     conn.execute(upsert_statement)
-    
 
-def load_dataframe(df, table_name="transactions"):
-    assert os.getenv("PG_CONN") is not None, "PG_CONN environment variable is not set."
-    pg_conn = os.getenv("PG_CONN")
+
+def load_dataframe(df, pg_conn=None, table_name="transactions"):
+    if pg_conn is None:
+        assert os.getenv("PG_CONN") is not None, "PG_CONN environment variable is not set."
+        pg_conn = os.getenv("PG_CONN")
 
     df.to_sql(name=table_name, con=pg_conn, if_exists="append", method=postgres_upsert, index=False, dtype={"additional_data": JSONB})
