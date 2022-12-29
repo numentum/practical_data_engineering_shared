becbbc86d6bca0189ca273b257d4175a03b44abb
Simon Szalai <simon.szalai@gmail.com>
2022-12-29 10:29:42 -0300

    modify for J3

--- io/extract.py
+++ io/extract.py
@@ -11,7 +11,14 @@ def extract_table(table_name, pg_conn=None, start_time=None, end_time=None):
     query = f"SELECT * FROM {table_name}"
 
     if start_time is not None and end_time is not None:
-        query += f"WHERE created_at >= '{start_time}' AND created_at < '{end_time}'"
+        if table_name == "online_transactions":
+            start_timestamp = pd.to_datetime(start_time).timestamp()
+            end_timestamp = pd.to_datetime(end_time).timestamp()
+
+            query += f" WHERE stripe_data ->> 'created' >= '{start_timestamp}' AND stripe_data ->> 'created' < '{end_timestamp}'"
+
+        else:
+            query += f" WHERE created_at >= '{start_time}' AND created_at < '{end_time}'"
 
     df = pd.read_sql(
         sql=query,
