442f46515ad16d37d6d83848f922576ea875729c
Simon Szalai <simon.szalai@gmail.com>
2023-01-26 13:12:34 -0300

    add comments

--- io/extract.py
+++ io/extract.py
@@ -2,8 +2,35 @@ import os
 import pandas as pd
 
 
-def extract_table(table_name, pg_conn=None, start_time=None, end_time=None):
+def extract_table(table_name, pg_conn=None, start_time=None, end_time=None, context=None):
+    """
+    Params
+    ------
+    table_name : str
+        Name of the PostgreSQL table to extract. The PostgreSQL connection is loaded from the `PG_CONN` environment variable.
 
+    pg_conn : str, optional
+        Optional PostgreSQL connection string. If not provided, PG_CONN environment variable is used.
+
+    start_time : str, optional
+        Datetime formatted as `%Y-%d-%m`. If provided, only records with a timestamp greater than or equal to this value are returned. Used for partitioning.
+
+    end_time : str, optional
+        Datetime formatted as `%Y-%d-%m`. If provided, only records with a timestamp smaller than or equal to this value are returned. Used for partitioning.
+
+    context : dagster.core.execution.context.compute.ComputeExecutionContext, optional
+        Dagster context object. Used to log information about the processing.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        Pandas DataFrame containing the whole PostgreSQL table.
+
+    Raises
+    ------
+    AssertionError
+        If the PG_CONN environment variable is not set.
+    """
     if pg_conn is None:
         assert os.getenv("PG_CONN") is not None, "PG_CONN environment variable is not set."
         pg_conn = os.getenv("PG_CONN")
--- io/load.py
+++ io/load.py
@@ -19,6 +19,30 @@ def postgres_upsert(table, conn, keys, data_iter):
 
 
 def load_dataframe(df, pg_conn=None, table_name="transactions", context=None):
+    """
+    Inserts a DataFrame to a postgres table in a way that if there is already a record with a matching primary key,
+    it will overwrite the record with the new data. This makes it possible to repeatedly call this function to update
+    the processed data in case the processing logic changed, without duplicating rows.
+
+    Params
+    ------
+    df : pd.DataFrame
+        Pandas DataFrame to be saved to the `transactions` table.
+
+    pg_conn : str, optional
+        Optional PostgreSQL connection string. If not provided, PG_CONN environment variable is used.
+
+    table_name : str, optional
+        Name of the PostgreSQL table to load the data to. Defaults to `transactions`.
+
+    context : dagster.core.execution.context.compute.ComputeExecutionContext, optional
+        Dagster context object. Used to log information about the processing.
+
+    Raises
+    ------
+    AssertionError
+        If the PG_CONN environment variable is not set.
+    """
     if pg_conn is None:
         assert os.getenv("PG_CONN") is not None, "PG_CONN environment variable is not set."
         pg_conn = os.getenv("PG_CONN")
